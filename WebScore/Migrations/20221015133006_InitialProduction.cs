e254a3f9b60540b288f457441d1990c9ff15471d
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-10-15 10:32:23 -0300

    initialproduction

---
+++ WebScore/Migrations/20221015133006_InitialProduction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebScore.Migrations
+{
+    public partial class InitialProduction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
