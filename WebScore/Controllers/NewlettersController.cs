3573055e91d0ff9bfb340bd0f5f21ac5e5d73b5e
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2022-10-27 16:11:07 -0300

    criando outros layouts

---
+++ WebScore/Controllers/NewlettersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebScore.Controllers
+{
+    public class NewlettersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Newletter()
+        {
+            ViewBag.Title = "New Letters";
+            return View();
+        }
+    }
+}
